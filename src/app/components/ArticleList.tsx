f044e7f14eec1d9f19d4ccc4012fe1862ee4b1ee
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-10-15 23:20:40 +0900

    section24まで

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,73 +1,45 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Article } from "@/types";
 
-export const ArticleList = () => {
+type ArticleListProps = {
+  articles: Article[];
+};
+
+export const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://cdn.pixabay.com/photo/2025/07/20/08/07/alley-9723861_1280.jpg"
-            alt=""
-            width={660}
-            height={100}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
+      {articles.map((article) => (
+        <article className="shadow my-4 flex flex-col" key={article.id}>
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
+            <Image
+              src="https://cdn.pixabay.com/photo/2025/07/20/08/07/alley-9723861_1280.jpg"
+              alt=""
+              width={660}
+              height={100}
+            />
           </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
-          >
-            Next.js勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">By takakichi,2025/10/14</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            maiores accusamus unde incidunt rerum esse at laboriosam aliquid,
-            earum fuga laborum magnam porro neque. Maxime eius deleniti ducimus
-            facilis obcaecati?
-          </Link>
-          <Link href="#" className="hover:text-black text-pink-800">
-            続きを読む
-          </Link>
-        </div>
-      </article>
-
-      <article className="shadow my-4 flex flex-col">
-        <Link href="#" className="hover:opacity-75">
-          <Image
-            src="https://cdn.pixabay.com/photo/2025/07/20/08/07/alley-9723861_1280.jpg"
-            alt=""
-            width={660}
-            height={100}
-          />
-        </Link>
-        <div className="bg-white flex flex-col justify-start p-6">
-          <Link href="#" className="text-blue-700 pb-4 font-bold">
-            Technology
-          </Link>
-          <Link
-            href="#"
-            className="text-slate-900 text-3xl font-bold hover:text-gray-700"
-          >
-            Next.js勉強中
-          </Link>
-          <p className="text-sm pb-3 text-slate-900">By takakichi,2025/10/14</p>
-          <Link href="#" className="text-slate-900 pb-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            maiores accusamus unde incidunt rerum esse at laboriosam aliquid,
-            earum fuga laborum magnam porro neque. Maxime eius deleniti ducimus
-            facilis obcaecati?
-          </Link>
-          <Link href="#" className="hover:text-black text-pink-800">
-            続きを読む
-          </Link>
-        </div>
-      </article>
+          <div className="bg-white flex flex-col justify-start p-6">
+            <Link href="#" className="text-blue-700 pb-4 font-bold">
+              Technology
+            </Link>
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 text-3xl font-bold hover:text-gray-700"
+            >
+              {article.title}
+            </Link>
+            <p className="text-sm pb-3 text-slate-900">{article.createdAt}</p>
+            <Link href={`articles/${article.id}`} className="text-slate-900 pb-6">
+              {article.content}
+            </Link>
+            <Link href={`articles/${article.id}`} className="hover:text-black text-pink-800">
+              続きを読む
+            </Link>
+          </div>
+        </article>
+      ))}
     </div>
   );
 };
